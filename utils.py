7423d3f30f6f56d3416e2c3eb46a1071cdf81a39
mjh244 <54736526+mjh244@users.noreply.github.com>
2020-12-11 16:19:23 -0500

    Uploading Genetic Dataset
    10 genetic datasets as well as extra code for util.py to read them in.

--- utils.py
+++ utils.py
@@ -23,8 +23,18 @@ str_cols = [
     'Patient\'s Vital Status'
 ]
 
+# Reads in datasets
 df = pd.read_csv('./dl-classifier/brca-data.csv')
-print(df.shape)
+#df = pd.read_csv('./dl-classifier/CDH1-dataset.csv')
+#df = pd.read_csv('./dl-classifier/GATA3-dataset.csv')
+#df = pd.read_csv('./dl-classifier/KMT2C-dataset.csv')
+#df = pd.read_csv('./dl-classifier/KMT2D-dataset.csv')
+#df = pd.read_csv('./dl-classifier/MAP3K1-dataset.csv')
+#df = pd.read_csv('./dl-classifier/NOTCH1-dataset.csv')
+#df = pd.read_csv('./dl-classifier/PDE4DIP-dataset.csv')
+#df = pd.read_csv('./dl-classifier/PIK3CA-dataset.csv')
+#df = pd.read_csv('./dl-classifier/TBX3-dataset.csv')
+#df = pd.read_csv('./dl-classifier/TP53-dataset.csv')
 
 # Drop rows which are missing data
 df.replace('NA', np.nan)
@@ -34,6 +44,7 @@ df = df.dropna()
 df = df[df['Patient\'s Vital Status'] != 'Died of Other Causes']
 print(df.shape)
 
+print(df)
 # Map nominal features to numbers
 for col in str_cols:
     df[col] = pd.Categorical(df[col], ordered=True).codes
@@ -47,4 +58,15 @@ scaled = min_max_scaler.fit_transform(np_df)
 # Extract final df
 norm_df = pd.DataFrame(scaled)
 norm_df.to_csv('./dl-classifier/brca-normalized.csv')
+# For genetic datasets
+#norm_df.to_csv('./dl-classifier/CDH1-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/GATA3-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/KMT2C-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/KMT2D-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/MAP3K1-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/NOTCH1-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/PDE4DIP-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/PIK3CA-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/TBX3-normalized.csv', index=False)
+#norm_df.to_csv('./dl-classifier/TP53-normalized.csv', index=False)
 print(norm_df)
